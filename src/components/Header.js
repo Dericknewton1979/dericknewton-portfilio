bd31ef77ed315dbad4f315d26b3e5a4003790739
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-05 14:30:54 -0700

    set links in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,54 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import Mylogo from "../app/assets/img/Mylogo.PNG";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return(
         <Navbar dark color='primary' sticky='top' expand= 'md'>
-            <NavbarBrand href='/'>
-                <img className="my-logo" src={Mylogo} alt='derick logo' />
+            <NavbarBrand className='ms-5' href='/'>
+                <img src={Mylogo} alt='derick logo' className=''  />
             </NavbarBrand>
+
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+        <Nav className = 'ms-auto' navbar>
+            
+                <NavItem>
+                  
+                    <NavLink className='nav-link' to='/About'>
+                         About
+                    </NavLink>
+                </NavItem>
+                
+                <NavItem>
+                    <NavLink className='nav-link' to='/Experience'>
+                         Experience
+                    </NavLink>
+                </NavItem>
+                
+                <NavItem>
+                    <NavLink className='nav-link' to='/Projects'>
+                         Projects
+                    </NavLink>
+                </NavItem>
+                
+                <NavItem>
+                    <NavLink className='nav-link' to='/Contact'>
+                         Contact
+                    </NavLink>
+                </NavItem>
+        </Nav>
+
+      </Collapse>
         </Navbar>
     );
 };
