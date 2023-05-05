d91951641af3419bacb68cda6e0e2b00ceda57f4
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-05 11:31:41 -0700

    create the header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from "react";
 import Home from "./pages/Home";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import Mylogo from "./app/assets/img/Mylogo.PNG";
 import './App.css';
+import Header from "./components/Header";
 
 
 
@@ -10,13 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand= 'md'>
-            <Container>
-                <NavbarBrand href='/'>
-                      <img className="my-logo" src={Mylogo} alt='derick logo' />
-                </NavbarBrand>
-            </Container>
-        </Navbar>
+        <Header />
         <Home/>
     </div>
     
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,22 @@
+import { Navbar, NavbarBrand } from "reactstrap";
+import Mylogo from "../app/assets/img/Mylogo.PNG";
+
+const Header = () => {
+    return(
+        <Navbar dark color='primary' sticky='top' expand= 'md'>
+            <NavbarBrand href='/'>
+                <img className="my-logo" src={Mylogo} alt='derick logo' />
+            </NavbarBrand>
+        </Navbar>
+    );
+};
+
+
+export default Header;
+
+
+
+
 // import React from "react";
 
 // function Header() {
