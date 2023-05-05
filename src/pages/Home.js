0a0c7c6fb840cb5984138ee9cea43c4c117d893e
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-04 18:27:51 -0700

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
+        <div className="main">
         {/* <Header /> */}
         <h1 className="name">Derick Newton</h1>
         </div>
