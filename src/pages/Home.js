ec5cc0dda3d7e72215d317505828b7c0fb57eec9
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-09 16:14:49 -0700

    Home screen sliding text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,24 @@ function Home() {
   return (
     <div className="main">
       <h1 className="name slide-in-left">Derick Newton</h1>
-      <h2 className="statement slide-in-right">
+
+      <h2
+        className="statement slide-in-right-delayed "
+        style={{ animationDelay: "1s" }}
+      >
         Making the web a better place!
       </h2>
+      <div>
+        <h3
+          className="my-info slide-in-left-delayed"
+          style={{ animationDelay: "1s" }}
+        >
+          Im a full stack engineer with a passion for perfection. After many
+          years in Business Management I am pursuing a career in web
+          development. I am currently open to any opportunities and cant wait to
+          help out!
+        </h3>
+      </div>
     </div>
   );
 }
