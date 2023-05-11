5dde912c336c1b7a5a9dbfe92be283ea796822a6
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-11 12:28:44 -0700

    styling for logos on experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,45 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
 import logo1 from "../app/assets/img/logo.png";
 import logo2 from "../app/assets/img/1800gotjunk-logo.png";
 import logo3 from "../app/assets/img/fred-meyer-logo2.png";
+import Mylogo from "../app/assets/img/Mylogo.PNG";
 
 const Experience = () => {
+  const [activeCard, setActiveCard] = useState(null);
+
+  const handleClick = (cardName) => {
+    if (activeCard === cardName) {
+      setActiveCard(null);
+    } else {
+      setActiveCard(cardName);
+    }
+  };
+
   return (
     <div>
       <Container>
         <div className="experience-info">
           <h1>Experience:</h1>
         </div>
-        <CardBody className="nucamp">
-          <img src={logo1} alt="nucamp logo" />
-          <ul>
-            <li>test 1</li>
-            <li>test 2</li>
-            <li>test 3</li>
-            <li>test 4</li>
-          </ul>
-        </CardBody>
+        <ul className="job-links">
+          <li onClick={() => handleClick("nucamp")}>Nucamp</li>
+          <li onClick={() => handleClick("junk")}>1-800-GOT-JUNK?</li>
+          <li onClick={() => handleClick("fredmeyer")}>FredMeyer</li>
+        </ul>
 
-        <CardBody className="junk">
-          <img src={logo2} alt="got junk logo" />
-          <ul>
-            <li>test 1</li>
-            <li>test 2</li>
-            <li>test 3</li>
-            <li>test 4</li>
-          </ul>
-        </CardBody>
+        <div className="jobs">
+          {activeCard === "nucamp" && (
+            <CardBody className="nucamp">
+              <img src={logo1} alt="nucamp logo" />
+              <ul>
+                <li>test 1</li>
+                <li>test 2</li>
+                <li>test 3</li>
+                <li>test 4</li>
+              </ul>
+            </CardBody>
+          )}
 
-        <CardBody className="fredmeyer">
-          <img src={logo3} alt="fredmeyer logo" />
-          <ul>
-            <li>test 1</li>
-            <li>test 2</li>
-            <li>test 3</li>
-            <li>test 4</li>
-          </ul>
-        </CardBody>
+          {activeCard === "junk" && (
+            <CardBody className="junk">
+              <img src={logo2} alt="got junk logo" />
+              <ul>
+                <li>test 1</li>
+                <li>test 2</li>
+                <li>test 3</li>
+                <li>test 4</li>
+              </ul>
+            </CardBody>
+          )}
+
+          {activeCard === "fredmeyer" && (
+            <CardBody className="fredmeyer">
+              <img src={logo3} alt="fredmeyer logo" />
+              <ul>
+                <li>test 1</li>
+                <li>test 2</li>
+                <li>test 3</li>
+                <li>test 4</li>
+              </ul>
+            </CardBody>
+          )}
+          {activeCard === null && (
+            <CardBody className="derick-newton">
+              <img src={Mylogo} alt="Derick Newton Logo" />
+            </CardBody>
+          )}
+        </div>
       </Container>
     </div>
   );
